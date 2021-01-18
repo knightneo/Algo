f3553f4a29ae2c15e1c80335e51efa719916495d
knightneo <knightneogarden@gmail.com>
2021-01-18 22:35:23 +0800

    nothing

--- C01_sort.py
+++ C01_sort.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
-# -*- coding:utf8 -*-
-
-#插入排序(Insertion Sort)
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        j = i - 1
-        key = arr[i]
-        while j >= 0 and key < arr[j]:
-            arr[j+1] = arr[j]
-            j-=1
-        arr[j+1] = key
-
-
-arr = [5,2,3,1,4]
-insertionSort(arr)
-print('sorted array:')
-for i in range(len(arr)):
-    print("%d" %arr[i])
-        
+##!/usr/bin/python
+## -*- coding:utf8 -*-
+#
+##插入排序(Insertion Sort)
+#def insertionSort(arr):
+#    for i in range(1, len(arr)):
+#        j = i - 1
+#        key = arr[i]
+#        while j >= 0 and key < arr[j]:
+#            arr[j+1] = arr[j]
+#            j-=1
+#        arr[j+1] = key
+#
+#
+#arr = [5,2,3,1,4]
+#insertionSort(arr)
+#print('sorted array:')
+#for i in range(len(arr)):
+#    print("%d" %arr[i])
+#        
